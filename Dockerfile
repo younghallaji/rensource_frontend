08928fe052d87eb31c772d6eb997b36ce1a05556
younghallaji <younghallajinoni@gmail.com>
2022-10-01 09:33:54 +0100

    next page fixed

--- Dockerfile
+++ Dockerfile
@@ -12,5 +12,5 @@ COPY config/nginx.conf /etc/nginx/conf.d/default.conf
 COPY config/rensourcecert.crt /etc/nginx/certs/rensourcecert.crt
 COPY config/rensourcecert.key /etc/nginx/certs/rensourcecert.key
 
-EXPOSE 80
+EXPOSE 8000
 CMD ["nginx", "-g", "daemon off;"]
--- src/components/SingleCaseStudy/SingleCaseStudyOne.jsx
+++ src/components/SingleCaseStudy/SingleCaseStudyOne.jsx
@@ -8,10 +8,22 @@ import images from '../../constants/images';
 import { Link, useParams } from 'react-router-dom';
 import useFetch from '../../constants/useFetch';
 import { Url } from '../../constants/baseurl';
+import { useState, useEffect } from 'react';
 
 const SingleCaseStudyOne = () => {
   const {id} = useParams();
+  const [next, setNext] = useState("")
   const {data:info, loading, error} = useFetch(Url+'Projects/Project?Id='+id)
+  useEffect(()=>{
+    const getPress = async () =>{
+      const res = await fetch(Url+'Projects/AllProjects?pageNumber=1&pageSize=6');
+      const data = await res.json()
+      let nextPrj = data.data.filter((project) => project.id != id && project.projectOverview != "")
+      setNext(nextPrj);
+      console.log(nextPrj)
+    }
+    getPress()
+  }, [])
   
   return (
     <>
@@ -45,22 +57,33 @@ const SingleCaseStudyOne = () => {
             <div className="next">
                 <p className="solution-title">Next Project</p>
             </div>
-            <Link to={'/case-one-details'}>
-            <Card className='col next-details col-md-6 col-sm-12'>
-                <div className="row">
-                    <div className="col next-case-img col-sm-4 m-4">
-                        <img src={images.nextCaseOne} alt="" />
-                    </div>
+            
+              {
+                (next == "") ? 
+                <Spinner animation="border" role="status">
+                  <span className="visually-hidden">Loading...</span>
+              </Spinner> : 
+                <a href={'/project/'+next[1].id}>
+                <Card className='col next-details col-md-6 col-sm-12'>
+              <div className="row">
+                  <div className="col next-case-img col-sm-4 m-4">
+                      <img src={next[1].image} alt="" />
+                  </div>
 
-                    <div className="col col-sm-7 next-content">
-                        <p style={{marginTop:'24px'}} className="solution-title">Powering Premium Poultry Farm</p>
-                        <p className="next-content">
-                        Learn how Rensource Energy helped Premium Poultry Farm Enhance its Production Effort and Provide Visibility on its Energy Consumption & Cost of Energy 
-                        </p>
-                    </div>
-                </div> 
-            </Card>
-            </Link>
+                  <div className="col col-sm-7 next-content">
+                      <p style={{marginTop:'24px'}} className="solution-title">{next[1].title}</p>
+                      <p className="next-content">
+                        {next[1].description}
+                      </p>
+                  </div>
+              </div> 
+          </Card>
+          </a>
+              }
+            
+            
+            
+            
           </Container>
         <Footer />
     </>
